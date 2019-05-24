e549a1bc1614d5cf1eaf5e3499c978802ab5a9af
unknown <jmanuelsanchezros@gmail.com>
2019-05-24 21:00:31 +0200

    Base de datos actualizada

--- src/database.js
+++ src/database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://root:<naruto56>@cluster0-i1y5t.mongodb.net/test?retryWrites=true', {
-  useCreateIndex: true,
-  useNewUrlParser: true
-})
-  .then(db => console.log('DB is connected'))
-  .catch(err => console.error(err));
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://root:<naruto56>@cluster0-i1y5t.mongodb.net/test?retryWrites=true";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
