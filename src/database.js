56d6be99f3f1288bc445e079a5b9eeea0183d8c3
unknown <jmanuelsanchezros@gmail.com>
2019-05-24 20:40:21 +0200

    Base de datos agregada

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://localhost/node-notes-db', {
+mongoose.connect('mongodb+srv://root:<password>@cluster0-i1y5t.mongodb.net/test?retryWrites=true', {
   useCreateIndex: true,
   useNewUrlParser: true
 })
