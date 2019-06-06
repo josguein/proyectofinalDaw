deaa2f718fce76cca85d7f22552104a2f91a527c
unknown <jmanuelsanchezros@gmail.com>
2019-06-06 04:58:04 +0200

    make it better

--- src/database.js
+++ src/database.js
@@ -1,12 +1,9 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://root:<naruto56>@cluster0-i1y5t.mongodb.net/test?retryWrites=true";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-module.exports=client;
+mongoose.connect('mongodb+srv://jose:<naruto56>@cluster0-i1y5t.mongodb.net/test?retryWrites=true&w=majority', {
+  useCreateIndex: true,
+  useNewUrlParser: true
+})
+  .then(db => console.log('DB is connected'))
+  .catch(err => console.error(err));
