f122c56be60cc0ff5988d81365f1e52e6b5ee34d
unknown <jmanuelsanchezros@gmail.com>
2019-06-10 03:13:15 +0200

    commit

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb://root:1234@cluster0-shard-00-00-i1y5t.mongodb.net:27017,cluster0-shard-00-01-i1y5t.mongodb.net:27017,cluster0-shard-00-02-i1y5t.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('mongodb://admin:x7GRvTDSycf9RufC@SG-cluster1-21859.servers.mongodirector.com:27017/admin', {
   useCreateIndex: true,
   useNewUrlParser: true
 })
