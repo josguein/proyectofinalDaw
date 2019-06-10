fc4b4c7c18d128184448f41989c4184dc1a8839c
unknown <jmanuelsanchezros@gmail.com>
2019-06-10 02:52:44 +0200

    a3

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.set('useFindAndModify', false);
-mongoose.connect('mongodb+srv://root:1234@cluster0-i1y5t.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb://root:1234@cluster0-shard-00-00-i1y5t.mongodb.net:27017,cluster0-shard-00-01-i1y5t.mongodb.net:27017,cluster0-shard-00-02-i1y5t.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useCreateIndex: true,
   useNewUrlParser: true
 })
